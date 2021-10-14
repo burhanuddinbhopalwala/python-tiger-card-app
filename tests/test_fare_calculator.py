ede435b161b5f09c576a7efd761c6dee3cb76792
Burhanuddin Bhopalwala <bbhopalw+github@gmail.com>
2021-10-14 12:05:07 +0530

    add: tests file

--- tests/test_fare_calculator.py
+++ tests/test_fare_calculator.py
@@ -18,13 +18,13 @@ class TestJourneyFare(unittest.TestCase):
         # * Daily journies - Use Case 1
         self.test_day_journies = [['Monday', 10.20, 2, 1], ['Monday', 10.45, 1, 1],
                                   ['Monday', 16.15, 1, 1], ['Monday', 18.15, 1, 1], ['Monday', 19.00, 1, 2]]
-        self.exact_day_fare = 95  # * Please refer  the feeback README.md
+        self.exact_day_fare = 120  # * Please refer  the feeback README.md
 
         # * Weekly journies - Use Case 2
         self.test_weekly_journies = [['Monday', 120, 1, 2], ['Tuesday', 120, 1, 2],
                                      ['Wednesday', 120, 1, 2], ['Thursday', 120, 1, 2], ['Friday', 80, 1, 1],
                                      ['Saturday', 40, 1, 2], ['Sunday', 40, 1, 2], ['Monday', 100, 1, 2]]
-        self.exact_weekly_fare = 600  # * Please refer  the feeback README.md
+        self.exact_weekly_fare = 700  # * Please refer  the feeback README.md
 
     @pytest.mark.dailyfaretest
     def test_day_journies_fare(self):
